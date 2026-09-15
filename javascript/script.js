cf832ec37d37c721be2524131e1d11fd4cfb1665
gterbang765-maker <gterbang765@gmail.com>
2026-09-15 19:13:39 +0800

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+const contactForm = document.getElementById("contactForm");
+const formMessage = document.getElementById("formMessage");
+
+contactForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+
+    if (name === "") {
+        formMessage.textContent = "Please enter your name.";
+        return;
+    }
+
+    formMessage.textContent =
+        "Thank you, " + name + "! Your message has sent.";
+
+});
